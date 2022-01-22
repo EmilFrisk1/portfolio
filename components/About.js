9e39f46850e06501e8f4ceedc353fefb87ee3b1c
Emil Frisk <incrassatum9876@gmail.com>
2022-01-22 14:09:48 +0200

    Fixed some padding issues with small screens

--- components/About.js
+++ components/About.js
@@ -6,8 +6,8 @@ export default function About() {
             <span className="section-separator"></span>
             <h1 className="section-header">About</h1>
             <div className="profile-container">
-               <img src="/images/profile_pic.jpg" alt="Profile picture" className="profile-pic" />
-               <p className="profile-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae porro velit perspiciatis sed asperiores laudantium incidunt ab expedita impedit cupiditate.</p>
+                <img src="/images/profile_pic.jpg" alt="Profile picture" className="profile-pic" />
+                <p className="profile-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae porro velit perspiciatis sed asperiores laudantium incidunt ab expedita impedit cupiditate.</p>
             </div>
         </div>
     )
