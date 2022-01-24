386bb62dd60a08632cf7b2f20d91218253a59035
Emil Frisk <incrassatum9876@gmail.com>
2022-01-24 10:55:44 +0200

    Added a title to the application

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import { AnimatePresence } from 'framer-motion'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <AnimatePresence exitBeforeEnter>
+      <Head key="head">
+        <title>Portfolio</title>
+      </Head>
       <Component {...pageProps} />
     </AnimatePresence>
   )
