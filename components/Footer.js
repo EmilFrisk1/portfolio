0436b7593059651cb92983af88ec37b58b88a5a5
Emil Frisk <incrassatum9876@gmail.com>
2022-05-20 14:09:55 +0300

    Edited the current phone number

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
-    return (
-        <footer className="main-footer">
-            <div className="container">
-                <div className="contact-phone">
-                    <p className='footer-title'>Phone number</p>
-                    <p className='footer-title'>+358-40 2578125</p>
-                </div>
-                <div className="contact-email">
-                    <p className='footer-title'>Email</p>
-                    <p className='footer-title'>friskemil2@gmail.com</p>
-                </div>
-            </div>
-        </footer>
-    )
+	return (
+		<footer className="main-footer">
+			<div className="container">
+				<div className="contact-phone">
+					<p className="footer-title">Phone number</p>
+					<p className="footer-title">+358-413116947</p>
+				</div>
+				<div className="contact-email">
+					<p className="footer-title">Email</p>
+					<p className="footer-title">friskemil2@gmail.com</p>
+				</div>
+			</div>
+		</footer>
+	);
 }
