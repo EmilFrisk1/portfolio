3dc3dc133bac6f95ccb1761cb5b5c395309edd3e
Emil Frisk <wallaton100@gmail.com>
2023-02-24 15:50:33 +0200

    added a new project

--- components/Projects.js
+++ components/Projects.js
@@ -1,82 +1,115 @@
-import React from 'react'
+import React from "react";
 
 export default function Projects() {
-    return (
-        <div id="projects" className='project-section container'>
-            <span className="section-separator"></span>
-            <h1 className="section-header test">Projects</h1>
-            <div className="project-row">
-                <a href="https://youtube-homepage-clone.herokuapp.com/home" target="_blank" rel="noreferrer">
-                    <div className="project-card">
-                        <img src="/images/Youtube-homepage-clone.JPG" alt="Project image" className="project-image" />
-                        <h3 className="project-title">Homepage</h3>
-                        <span className="section-separator center"></span>
-                        <p className="project-description">
-                        This project is made with CSS and vanlilla Javascript. It has tooltips, dark and lightmode and some hover effects. 
-                        </p>
-                        <p className="project-technologies">
-                            <div className="technologies">css</div>
-                            <div className="technologies">node</div>
-                        </p>
-                    </div>
-                </a>
-                <a href="https://github.com/Anemoi4/Cart-API" target="_blank" rel="noreferrer">
-                    <div className="project-card">
-                        <img src="/images/cart.webp" alt="Project image" className="project-image" />
-                        <h3 className="project-title">Cart-API</h3>
-                        <span className="section-separator center"></span>
-                        <p className="project-description">
-                            This API allows you to create a cart, add items to the cart, change the quantity of items in the cart and remove an item from the cart.
-                        </p>
-                        <p className="project-technologies">
-                            <div className="technologies">Mongo</div>
-                            <div className="technologies">node</div>
-                        </p>
-                    </div>
-                </a>
-            </div>
-            <div className="project-row">
-                <a href="https://magic-keys.netlify.app/" target="_blank" rel="noreferrer">
-                    <div className="project-card">
-                        <img src="/images/E-commerce.JPG" alt="Project image" className="project-image" />
-                        <h3 className="project-title">E-Commerce</h3>
-                        <span className="section-separator center"></span>
-                        <p className="project-description">
-                            This project is full MERN stack project. It uses webhooks, sends emails to a customer and CSS supports phones.
-                        </p>
-                        <p className="project-technologies">
-                            <div className="technologies-row">
-                                    <div className="technologies">mongo</div>
-                                    <div className="technologies">express</div>
-                            </div>
-                            <div className="technologies-row">
-                                <div className="technologies">React</div>
-                                <div className="technologies">node</div>
-                            </div>
-                        </p>
-                    </div>
-                </a>
-                <a href="https://game-mania123.netlify.app/" target="_blank" rel="noreferrer">
-                    <div className="project-card">
-                        <img src="/images/Gamin-forum-app.JPG" alt="Project image" className="project-image" />
-                        <h3 className="project-title">Gaming Forum</h3>
-                        <span className="section-separator center"></span>
-                        <p className="project-description">
-                            This project has custom authentication system that uses JWT, custom error handling on the server and sorting options for the posts.
-                        </p>
-                        <p className="project-technologies">
-                            <div className="technologies-row">
-                                    <div className="technologies">mongo</div>
-                                    <div className="technologies">express</div>
-                                </div>
-                            <div className="technologies-row">
-                                <div className="technologies">React</div>
-                                <div className="technologies">node</div>
-                            </div>
-                        </p>
-                    </div>
-                </a>
-            </div>
-        </div>
-    )
+  return (
+    <div id="projects" className="project-section container">
+      <span className="section-separator"></span>
+      <h1 className="section-header test">Projects</h1>
+      <div className="project-row">
+        <a
+          href="https://youtube-homepage-clone.herokuapp.com/home"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="project-card">
+            <img
+              src="/images/Youtube-homepage-clone.JPG"
+              alt="Project image"
+              className="project-image"
+            />
+            <h3 className="project-title">Homepage</h3>
+            <span className="section-separator center"></span>
+            <p className="project-description">
+              This project is made with CSS and vanlilla Javascript. It has
+              tooltips, dark and lightmode and some hover effects.
+            </p>
+            <p className="project-technologies">
+              <div className="technologies">css</div>
+              <div className="technologies">node</div>
+            </p>
+          </div>
+        </a>
+        <a
+          href="https://github.com/Anemoi4/Cart-API"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="project-card">
+            <img
+              src="/images/cart.webp"
+              alt="Project image"
+              className="project-image"
+            />
+            <h3 className="project-title">Cart-API</h3>
+            <span className="section-separator center"></span>
+            <p className="project-description">
+              This API allows you to create a cart, add items to the cart,
+              change the quantity of items in the cart and remove an item from
+              the cart.
+            </p>
+            <p className="project-technologies">
+              <div className="technologies">Mongo</div>
+              <div className="technologies">node</div>
+            </p>
+          </div>
+        </a>
+      </div>
+      <div className="project-row">
+        <a
+          href="https://github.com/EmilFrisk1/Job-listing-api/tree/main"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="project-card">
+            <img
+              src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Flisting_411771&psig=AOvVaw1QmXntJKLfC1soBlOT3QNO&ust=1677332916338000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKCywpamrv0CFQAAAAAdAAAAABAE"
+              alt="Project image"
+              className="project-image"
+            />
+            <h3 className="project-title">Job listing backend</h3>
+            <span className="section-separator center"></span>
+            <p className="project-description">
+              This project is backend for job listings application.
+            </p>
+            <p className="project-technologies">
+              <div className="technologies-row">
+                <div className="technologies">postgreSQL</div>
+                <div className="technologies">express</div>
+              </div>
+            </p>
+          </div>
+        </a>
+        <a
+          href="https://game-mania123.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="project-card">
+            <img
+              src="/images/Gamin-forum-app.JPG"
+              alt="Project image"
+              className="project-image"
+            />
+            <h3 className="project-title">Gaming Forum</h3>
+            <span className="section-separator center"></span>
+            <p className="project-description">
+              This project has custom authentication system that uses JWT,
+              custom error handling on the server and sorting options for the
+              posts.
+            </p>
+            <p className="project-technologies">
+              <div className="technologies-row">
+                <div className="technologies">mongo</div>
+                <div className="technologies">express</div>
+              </div>
+              <div className="technologies-row">
+                <div className="technologies">React</div>
+                <div className="technologies">node</div>
+              </div>
+            </p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 }
